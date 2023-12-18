a7e7e4160ece98b6b4f46054c6768c6d02737419
Bett <bettpatrick080@gmail.com>
2023-12-18 23:52:15 +0300

    changed switch to if

--- gradeGenerator.js
+++ gradeGenerator.js
@@ -4,21 +4,17 @@ console.log((marks))
 alert ("Grade: " + gradingSystem(marks))
 
 function gradingSystem(marks){
-    switch (marks) {
-        case marks>79 && marks<=100:
-            return 'A'
-            break;
-        case marks>59 && marks<80:
-            return 'B'
-            break;
-        case marks>49 && marks<60:
-            return 'C'
-            break;
-        case marks>39 && marks<50:
-            return 'D'
-            break;
-        case marks<40:
-            return 'E'
-            break;
-    }
+    
+    if (marks>79 && marks<=100){return 'A'}
+        
+    else if(marks>59 && marks<80) {return 'B'}
+    
+    else if(marks>49 && marks<60) {return 'C'}
+    
+    else if(marks>39 && marks<50) {return 'D'}
+    
+    else if(marks<40) {return 'E'}
+
+    else{return "Invalid input!"}
+
 }
---
+++ salaryCalculator.js
@@ -0,0 +1,6 @@
+Write a program whose major task is to calculate an individual’s Net Salaryn by getting the inputs of basic salary and benefits. Calculate the payee (i.e. Tax), NHIF Deductions, NSSF Deductions, gross salary, and net salary. 
+
+
+
+let basicSalary = parseInt(prompt("Enter your basic salary: "))
+let bebefits = parseInt(prompt("Enter benefits: "))
