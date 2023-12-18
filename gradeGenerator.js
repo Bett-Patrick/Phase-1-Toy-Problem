595d7b19c0d6fcd0ea65cd3bde0bf7dfa51917ee
Bett <bettpatrick080@gmail.com>
2023-12-18 23:06:30 +0300

    Updated gradeGen and SpeedDetector

--- gradeGenerator.js
+++ gradeGenerator.js
@@ -1,29 +1,24 @@
 // Student Grade Generator
-let marks = prompt("Enter your marks");
-let grade = function gradingSystem(marks){
-    if (marks >= 0 && marks>=100) {
-        console.log(parseInt(marks))
-        switch (marks) {
-            case marks>79:
-                grade = 'A'
-                break;
-            case marks>59 && marks<80:
-                grade = 'B'
-                break;
-            case marks>49 && marks<60:
-                grade = 'C'
-                break;
-            case marks>39 && marks<50:
-                grade = 'D'
-                break;
-            case marks<40:
-                grade = 'E'
-                break;
-            
-            default:
-                
-                break;
-        }
-    return grade()
+let marks = parseInt(prompt("Enter your marks: "))
+console.log((marks))
+alert ("Grade: " + gradingSystem(marks))
+
+function gradingSystem(marks){
+    switch (marks) {
+        case marks>79 && marks<=100:
+            return 'A'
+            break;
+        case marks>59 && marks<80:
+            return 'B'
+            break;
+        case marks>49 && marks<60:
+            return 'C'
+            break;
+        case marks>39 && marks<50:
+            return 'D'
+            break;
+        case marks<40:
+            return 'E'
+            break;
     }
 }
--- speedDetector.js
+++ speedDetector.js
@@ -1,18 +1,18 @@
 // Speed Detector 
 let speed = parseInt(prompt("Speed"))
-
 console.log(speedDetector(speed))
+alert(speedDetector(speed))
 
 function speedDetector (speed) {
     let demeritPoints = Math.floor((speed-70)/5)
     if (speed<70) {
-        return "Ok"
+        return ("Speed is Ok")
     }
     else if (speed>=70) {
-        return ("Points: " +demeritPoints)
+        return (("Points: " +demeritPoints))
     }
     else if (demeritPoints>12) {
-        console.log("License Suspended")
+        return ("License Suspended")
     }
 }
 
