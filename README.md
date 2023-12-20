aa5032ba031f2b2517c89ca772626fcb6b42751c
Bett <bettpatrick080@gmail.com>
2023-12-20 15:37:43 +0300

    Updated PAYE calculator & README

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # Phase-1-Toy-Problem
 Student Grade Generator, Speed Detector and Net Salary Calculator
+
+## Challenges
+
+### Challenge 1: Student Grade Generator (Toy Problem)
+
+#### Description
+Write a function that prompts the user to input student marks. The input should be between 0 and 100. The output should correspond to the correct grade, as shown below:
+- A: Greater than 79
+- B: 60 to 79
+- C: 59 to 49
+- D: 40 to 49
+- E: Less than 40
+
+### Challenge 2: Speed Detector (Toy Problem)
+
+#### Description
+Write a program that takes the speed of a car as input. If the speed is less than 70, it should print "Ok." Otherwise, for every 5 km/s above the speed limit (70), it should give the driver one demerit point and print the total number of demerit points. If the driver gets more than 12 points, the function should print "License suspended."
+
+### Challenge 3: Net Salary Calculator (Toy Problem)
+
+#### Description
+Write a program to calculate an individual’s Net Salary by getting inputs of basic salary and benefits. Calculate the payee (i.e., Tax), NHIF Deductions, NSSF Deductions, gross salary, and net salary.
+
+## Functionalities
+
+### Challenge 1: Student Grade Generator
+
+1. Open the `gradeGenerator.js` file.
+2. Run the script to input student marks.
+3. Receive the corresponding grade as output.
+
+### Challenge 2: Speed Detector
+
+1. Open the `speedDetector.js` file.
+2. Run the script to input the car's speed.
+3. Receive the total demerit points and check for license suspension.
+
+### Challenge 3: Net Salary Calculator
+
+1. Open the `netSalaryCalculator.js` file.
+2. Run the script to input basic salary and benefits.
+3. Recieve payee, NHIF, NSSF, gross salary, and net salary.
--- salaryCalculator.js
+++ salaryCalculator.js
@@ -1,6 +1,88 @@
-Write a program whose major task is to calculate an individual’s Net Salaryn by getting the inputs of basic salary and benefits. Calculate the payee (i.e. Tax), NHIF Deductions, NSSF Deductions, gross salary, and net salary. 
-
+// Write a program whose major task is to 
+// calculate an individual’s *Net Salary* 
+// by getting the inputs of *basic salary* and *benefits*. 
+// Calculate the *payee* 
+// (i.e. Tax), NHIF Deductions, NSSF Deductions, gross salary, and net salary. 
 
 
 let basicSalary = parseInt(prompt("Enter your basic salary: "))
-let bebefits = parseInt(prompt("Enter benefits: "))
+let benefits = parseInt(prompt("Enter benefits: "))
+
+//Taxable Income(gross salary) = (gross salary + Allowances + all benefits)
+//All Deductions = (PAYE + *fringeBenefitTax* + NHIF + NSSF + Housing Levy)
+//Allowances(input)
+//gross salary(bs + benefits)
+//net salary = (gross salary - All deductions)
+
+
+        /**Solution**/
+let grossIncome = basicSalary + benefits
+
+//PAYE Rates Object:
+const payeRates = {
+        range1 : [0,24000,0.1],
+       
+        range2 : [24000,32333,0.25],
+        
+        range3 : [32333,500000,0.30],
+        
+        range4 : [500000,800000,0.325],
+        
+        range5 : [800000,grossIncome,0.35]
+}
+console.log(Object.keys(payeRates))
+console.log(Object.values(payeRates))
+
+
+let outerKeys = Object.keys(payeRates)
+console.log(outerKeys)//returns array of the keys
+
+let objectValues = []
+let objectValue = []
+let lowerLimit = []
+let higherLimit = []
+let taxRate = []
+
+//Traverse outerKeys array and clg its values
+for (let i = 0; i < outerKeys.length; i++) {
+        let objectKey = outerKeys[i]
+        objectValues.push(payeRates[objectKey])
+        lowerLimit.push(objectValues[i][0])
+        higherLimit.push(objectValues[i][1])
+        taxRate.push(objectValues[i][2])
+}
+
+console.log(objectValues)
+console.log(lowerLimit)
+console.log(higherLimit)
+console.log(taxRate)
+
+
+//payee calculator function:
+
+function payeeCalculator(taxableIncome){
+    //gross Income(input)
+    //range(higher range - lower range)
+    //TAX rate(%)
+    //Tax(range * Tax rate)
+    let paye = 0
+    for (let i = 0; i < 5; i++) {
+        if (grossIncome<=higherLimit[0])  {
+                paye = paye + parseFloat(grossIncome*taxRate[i])
+                break
+        }
+        else if (grossIncome<=higherLimit[i]) {
+                paye = paye + parseFloat((grossIncome-lowerLimit[i])*taxRate[i])
+                break 
+        }
+        else if (grossIncome>lowerLimit[i] && grossIncome>higherLimit[i]) {
+                paye = paye + parseFloat((higherLimit[i] - lowerLimit[i])*taxRate[i])
+        }
+        else{
+                console.log("Entries should be a positive number!")
+        }
+    }
+    return paye
+}
+console.log(payeeCalculator())
+
